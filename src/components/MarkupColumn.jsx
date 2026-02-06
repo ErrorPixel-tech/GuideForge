92a9f898e51c14fe69a50f87513725c6c562c870
German <fedosihingerman@yandex.ru>
2026-02-06 19:09:51 +0300

    save before adding trim

--- src/components/MarkupColumn.jsx
+++ src/components/MarkupColumn.jsx
@@ -5,10 +5,59 @@ import style from './MarkupColumn.module.scss';
 function MarkupColumn() {
   const inputs = useSelector((state) => state.inputs.items);
 
+  async function handleCopyClick(event) {
+    let code = inputs.reduce((acc, input) => {
+      if (input.type === "hr") {
+        return acc + `[${input.tag}][/${input.tag}]\n`;
+      }
+      if (input.type === "screenshot") {
+        return acc + `[table]\n[tr]\n[td]\nВставьтеСкриншотСюда\n[/td]\n[/tr][/table]\n`
+      }
+      if (input.type === "screenshot-horizontal") {
+        return acc + (
+          `[table]\n[tr]\n[td]\nВставьтеСкриншотСюда\n[/td]\n[td]\nВставьтеСкриншотСюда\n[/td]\n[/tr]\n[/table]\n`
+        )
+      }
+      // if (input.type === "code") {
+      //   return acc + (
+      //     <pre className={style.pre} key={input.id}>
+      //       [{input.tag}]{input.value}[/{input.tag}]
+      //     </pre>
+      //   )
+      // }
+      if (input.tag) {
+        return (
+          acc + "[" + input.tag + "]" + input.value + "[/" + input.tag + "]" + "\n"
+        )
+      }
+      return (
+        acc + (input.value || `\u00A0`) + "\n"
+      )
+
+      // return acc + input.value;
+    }, "");
+
+    code += "\n[i]Сделано с помощью SteamEditor![/i]\n";
+
+    try {
+      await navigator.clipboard.writeText(code);
+      // btn.textContent = 'Скопировано!';
+      event.target.textContent = 'Скопировано!';
+      // через 5 секунд вернуть назад
+      setTimeout(() => {
+        event.target.textContent = 'Скопировать';
+      }, 1000);
+    } catch (err) {
+      console.error('Не удалось скопировать: ', err);
+    }
+    return code;
+  }
+
   return (
     <div className="column">
       <div className="header">
         <h2>Разметка</h2>
+        <button onClick={handleCopyClick}>Скопировать</button>
       </div>
       <div className="markup-list">
         {inputs.map((input) => {
