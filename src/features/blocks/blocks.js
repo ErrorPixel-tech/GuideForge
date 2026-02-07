e06583f67ab6ed3773d636125ed32cfb1aa9d91e
German <fedosihingerman@yandex.ru>
2026-02-07 20:05:51 +0300

    BlockType added

--- src/features/blocks/blocks.js
+++ src/features/blocks/blocks.js
@@ -10,7 +10,16 @@ const blocksSlice = createSlice(
             addInput(state, action) {
                 const newId = Date.now().toString();
                 const isDisabled = action.payload?.isDisabled || false;
-                state.items.push({ id: newId, type: action.payload.type, value: '', className: action.payload.className, tag: action.payload.tag, isDisabled });
+                state.items.push(
+                    {
+                        id: newId,
+                        blockType: "inputBlock",
+                        type: action.payload.type,
+                        value: '', 
+                        className: action.payload.className, 
+                        tag: action.payload.tag, 
+                        isDisabled
+                    });
             },
             // addList(state, action) {
             //     const listId = Date.now().toString();
@@ -23,46 +32,46 @@ const blocksSlice = createSlice(
             //             { id: itemId, value: '' }
             //         ],
             //     });
-            },
-            updateInput(state, action) {
-                const { id, value } = action.payload;
-                const input = state.items.find(item => item.id === id);
-                if (input) {
-                    input.value = value;
-                }
-            },
-            formatAllBlocks(state) {
-                state.items = state.items.map((input) => {
-                    if (input.type === "code") {
-                        return input;
-                    }
-                    let cleaned = input.value.replace(/\s+/g, ' ').trim();
-                    return { ...input, value: cleaned };
-                });
-            },
-            removeBlock(state, action) {
-                state.items = state.items.filter((item) => item.id !== action.payload.id);
-            },
-            moveBlockDown(state, action) {
-                const index = action.payload.index;
-                let newIndex = index + 1;
-                if (newIndex >= state.items.length) { return; }
-                const holder = state.items[index];
-                state.items[index] = state.items[newIndex];
-                state.items[newIndex] = holder;
-            },
-            moveBlockUp(state, action) {
-                const index = action.payload.index;
-                let newIndex = index - 1;
-                if (newIndex < 0) { return; }
-                const holder = state.items[index];
-                state.items[index] = state.items[newIndex];
-                state.items[newIndex] = holder;
-            },
-            removeAllBlocks(state) {
-                state.items = [];
+        // },
+        updateInput(state, action) {
+            const { id, value } = action.payload;
+            const input = state.items.find(item => item.id === id);
+            if (input) {
+                input.value = value;
             }
+        },
+        formatAllBlocks(state) {
+            state.items = state.items.map((input) => {
+                if (input.type === "code") {
+                    return input;
+                }
+                let cleaned = input.value.replace(/\s+/g, ' ').trim();
+                return { ...input, value: cleaned };
+            });
+        },
+        removeBlock(state, action) {
+            state.items = state.items.filter((item) => item.id !== action.payload.id);
+        },
+        moveBlockDown(state, action) {
+            const index = action.payload.index;
+            let newIndex = index + 1;
+            if (newIndex >= state.items.length) { return; }
+            const holder = state.items[index];
+            state.items[index] = state.items[newIndex];
+            state.items[newIndex] = holder;
+        },
+        moveBlockUp(state, action) {
+            const index = action.payload.index;
+            let newIndex = index - 1;
+            if (newIndex < 0) { return; }
+            const holder = state.items[index];
+            state.items[index] = state.items[newIndex];
+            state.items[newIndex] = holder;
+        },
+        removeAllBlocks(state) {
+            state.items = [];
         }
+    }
     }
 );
 
